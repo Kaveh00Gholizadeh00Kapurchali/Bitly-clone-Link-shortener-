55b37e31f3c34c5219f1115a8d4dd2967f9b25d0
Kaveh Gholizadeh <Sobhan.Gholizadeh.K@gmail.com>
2024-08-03 11:58:53 +0330

    init commit

---
+++ links/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import index
+from .views import root_link
+from .views import add_link
+
+urlpatterns = [
+    path("", index, name="home"),
+    path("<str:link_slug>/", root_link, name="root-link"),
+    path("link/create/", add_link, name="create-link"),
+]
